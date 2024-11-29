0d516db023d66128ce210ff4794906f2929cfb49
redmybit <matthew.kosyanchuk@gmail.com>
2024-11-29 09:55:16 -0600

    update vector class

--- main.py
+++ main.py
@@ -1,5 +1,18 @@
 
 class Vector:
-	def __init__(self):
-		pass
+	def __init__(self, position : list = None) -> None:
+		self.position = position
 
+	def get(self):
+		return self.position
+		
+	def get_form(self):
+		return self.to_form(self.get())
+		
+	def to_form(self, vector):
+		magnitude = math.sqrt(sum(component ** 2 for component in vector))
+        
+        if magnitude == 0:
+            directions = [0 for _ in vector]
+        else:
+            directions = [component / magnitude for component in vector]
