6345b6978016146e69c8743e5f6f99e648f054bf
redmybit <matthew.kosyanchuk@gmail.com>
2024-11-30 10:41:39 -0600

    add __add__ and __repr__ methods to the vector class

--- main.py
+++ main.py
@@ -2,17 +2,17 @@ import math
 
 # infinite dimension vector class
 class Vector:
-    def __init__(self, position = [0, 0]) -> None:
-        self.position = position # set the vector's position
+    def __init__(self, components = [0, 0]) -> None:
+        self.components = components # set the vector's components
 	    
 	self.magnitude = 0
 	self.direction = []
 	    
 	self.updated() # update the vectors magnitude and direction
 
-    # updates the vector's magnitude and direction based off it's position
+    # updates the vector's magnitude and direction based off it's components
     def update(self):
-	    self.magnitude, self.direction = self.get(self.position) # get the vector's magintude and direction
+	    self.magnitude, self.direction = self.get(self.components) # get the vector's magintude and direction
 	
     # get the vector in magnitude and direction form
     def get(self, vector : list):
@@ -27,7 +27,22 @@ class Vector:
         
         return directions, magnitude
 
-    # get the vector's position based off a magnitude and direction
+    # get the vector's components based off a magnitude and direction
     def to(self, magnitude : float, directions : list):
-        vector = [magnitude * direction for direction in directions]
-        return vector
+        components = [magnitude * direction for direction in directions]
+        return components
+
+    # function for adding the vectors
+    def __add__(self, other):
+
+        # check if the vectors have the same length
+        if len(self.components) != len(other.components):
+            raise ValueError("Vectors must have the same number of dimensions to add.")
+
+        # Component-wise addition
+        added_components = [a + b for a, b in zip(self.components, other.components)]
+        return Vector(added_components)
+
+    # string-representation of a vector
+    def __repr__(self):
+        return f"Vector({self.components})"
