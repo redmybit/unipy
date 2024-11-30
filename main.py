888a5aa276945d373823016e6e4a2f1b875c44ba
redmybit <matthew.kosyanchuk@gmail.com>
2024-11-30 10:26:48 -0600

    reworked vector class

--- main.py
+++ main.py
@@ -1,20 +1,31 @@
-
 import math
 
 class Vector:
-	def __init__(self, position : list = None) -> None:
-		self.position = position
+    def __init__(self, position = [0, 0]) -> None:
+        self.position = position # set the vector's position
+	    
+	self.magnitude = 0
+	self.direction = []
+	    
+	self.updated() # update the vectors magnitude and direction
 
-	def get(self):
-		return self.position
-		
-	def get_form(self):
-		return self.to_form(self.get())
-		
-	def to_form(self, vector):
-		magnitude = math.sqrt(sum(component ** 2 for component in vector))
+    def update(self):
+	self.magnitude, self.direction = self.get(self.position) # get the vector's magintude and direction
+	
+    # get the vector in magnitude and direction form
+    def get(self, vector : list):
+        magnitude = math.sqrt(sum(component ** 2 for component in vector))
+    
+        # avoid division by zero
+        if magnitude == 0:
+            directions = [0 for _ in vector]
+        else:
+            # normalize each component to find directions
+            directions = [component / magnitude for component in vector]
         
-		if magnitude == 0:
-			directions = [0 for _ in vector]
-		else:
-			directions = [component / magnitude for component in vector]
+        return directions, magnitude
+
+    # get the vector's position based off a magnitude and direction
+    def to(self, magnitude : float, directions : list):
+        vector = [magnitude * direction for direction in directions]
+        return vector
